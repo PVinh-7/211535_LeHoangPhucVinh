c16b6cb86686b86de3681474b8718e8ce92308b1
phucvinh <lvinh77777@gmail.com>
2024-10-22 09:41:46 -0700

    Add shellc1.c, shellc2.c, and main.c with functions

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Khai báo các hàm
+void print_shellc1();
+void print_shellc2();
+
+int main() {
+    print_shellc1();
+    print_shellc2();
+    return 0;
+}
---
+++ shellc1.c
@@ -0,0 +1,6 @@
+
+#include <stdio.h>
+
+void print_shellc1() {
+    printf("This is shellc1\n");
+}
